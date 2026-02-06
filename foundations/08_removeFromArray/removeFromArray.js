d5417c82c30b2c07ac20530622c3fab6ad1fd456
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-02-06 17:38:14 -0600

    finished project.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, filNum) {
+    return arr.filter(element => element !== filNum);
 };
 
+console.log(removeFromArray([1, 2, 3, 4, 5, 6], 3));
+
+
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;
