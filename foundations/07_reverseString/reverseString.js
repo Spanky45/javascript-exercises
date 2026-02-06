8b2601a17addeb64cc2f9dcef6d623e5afc36cbe
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-02-05 22:01:48 -0600

    finished.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
 const reverseString = function() {
+const str = 'hello there';
+const arr = str.split(' ');
 
+for (let i = 0; i < arr.length; i++)
+    arr[i] = arr[i].split('').reverse().join('')
+return arr.reverse().join(' ');
 };
-
+console.log(reverseString());
 // Do not edit below this line
-module.exports = reverseString;
+// module.exports = reverseString;
