606bca64369e2cc889bf968e70f26e56e1accefb
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-02-08 16:07:28 -0600

    finished project. i did good on this one.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let start
+    let end
+    let sum = 0
 
+    if (num1 > num2) { 
+        start = num2;
+        end = num1;
+    }
+    else if (num1 < num2) {
+        start = num1;
+        end = num2;
+    }
+    else {
+        return num1;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+        
+    }
+    return sum;
 };
 
+console.log(sumAll(3, 8));
+
 // Do not edit below this line
-module.exports = sumAll;
+// module.exports = sumAll;
