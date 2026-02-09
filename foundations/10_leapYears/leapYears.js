411469924c86fe65ad58b479f8c060a2acb2231d
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-02-09 14:36:03 -0600

    complete.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 !== 0 && year % 400 === 0) {
+        return 'Leap Year.'
+    }
+    else {
+        return 'Not a Leap Year.'
+    }
 };
-
+console.log(leapYears(2000))
 // Do not edit below this line
-module.exports = leapYears;
+// module.exports = leapYears;
