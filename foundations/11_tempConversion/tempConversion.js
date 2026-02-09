b2045668acdfd577de61d5ebd9b16a4bec30dcf2
Spanky45 <245573075+Spanky45@users.noreply.github.com>
2026-02-09 16:13:47 -0600

    completed.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  return Math.round(((far - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(76));
+
+const convertToFahrenheit = function(cel) {
+  return Math.round(((cel * 9/5) + 32) * 10) / 10;
 };
+
+console.log(convertToFahrenheit(24))
 
 // Do not edit below this line
-module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
-};
+// module.exports = {
+//   convertToCelsius,
+//   convertToFahrenheit
+// };
